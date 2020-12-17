4684e051ce93f71c7f23be172fd9cc4f1a5ce14b
Vitohhh <victorhfeliciano1010@outlook.com>
2020-12-17 20:03:59 -0300

    Adicionando métodos de adicionar pedidos e totalizá-los

--- Pedido.cs
+++ Pedido.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 namespace project
 {
+
     public class Pedido
     {
+        private List<ItemPedido> lista = new List<ItemPedido>();
+        
+        public void adicionar(ItemPedido itemPedido)
+        {
+            lista.Add(itemPedido);
+        }
 
+        public double totalizar ()
+        {
+            double total = 0;
+            for(int i=0; i < lista.Count; i++)
+            {
+                total = total + lista[i].quantidade;
+            }
+
+            return total;
+        }
     }
 }
