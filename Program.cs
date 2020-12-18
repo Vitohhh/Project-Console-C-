3dddb28ee9645c8e236011efcd843fbf343f52ea
Vitohhh <victorhfeliciano1010@outlook.com>
2020-12-17 22:01:49 -0300

    Sistema de perguntas e respostas ao usuário.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string descricao;
+            Console.WriteLine("Informe a descrição do produto:");
+            descricao = Console.ReadLine();
+
+            int valor;
+            Console.WriteLine("Informe o valor do produto:");
+            valor = int.Parse(Console.ReadLine());
+
+            double quantidade;
+            Console.WriteLine("Informe a quantidade do produto.");
+            quantidade = double.Parse(Console.ReadLine());
+        }
+    }
+}
